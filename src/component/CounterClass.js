78f894554b11f770ebc0c7e52709b3d52f5628b3
Stolyarov <t9818184233@gmailcom>
2021-04-11 20:06:46 +0300

    1

--- src/component/CounterClass.js
+++ src/component/CounterClass.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import { observable, makeObservable, action } from 'mobx';
 
-export default class CounterClass extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: this.props.initialCount ?? 0,
-    };
-    this.inc = this.inc.bind(this);
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.dec}>-</button>
-        <span>{this.state.count}</span>
-        <button onClick={this.inc}>+</button>
-      </div>
-    );
-  }
+// @observer
+const CounterClass = observer(
+  class extends Component {
+    //@observable
+    count = 0;
+    constructor(props) {
+      super(props);
+      makeObservable(this, {
+        count: observable,
+        dec: action,
+        inc: action.bound,
+      });
+      // значение по умолчанию
+      this.count = this.props.initialCount ?? 0;
+    }
+    render() {
+      return (
+        <div>
+          <button onClick={this.dec}>-</button>
+          <span>{this.count}</span>
+          <button onClick={this.inc}>+</button>
+        </div>
+      );
+    }
+    //@action (т.к. не потерян контекст)
+    dec = () => this.count--;
 
-  dec = () => this.setState(prevState => ({ count: prevState.count - 1 }));
-
-  inc() {
-    this.setState(prevState => ({ count: prevState.count + 1 }));
+    //@action.bound (т.к. потерян контекст)
+    inc() {
+      this.count++;
+    }
   }
-}
+);
+
+export default CounterClass;
